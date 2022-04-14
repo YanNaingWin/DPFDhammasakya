c315ae0c497b3fb581b58f5b2e6631f6947dfc06
Yan Naing Win <yan.naing.win@studioamk.com>
2022-04-14 16:38:36 +0630

    Added project

---
+++ src/settings.c80de.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/mainScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",main:"c8d50"}};
